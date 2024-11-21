5ff0cf5a4490b745f14f404345b58a8a45eef660
Kirill <35264591+kirill-84@users.noreply.github.com>
2024-11-21 09:53:25 +0300

    Update App.tsx
    Update code.

--- src/App.tsx
+++ src/App.tsx
@@ -27,15 +27,15 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
             {/* Here we add our button with alert callback */}
             <div className="card">
                 <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
                     Show Alert
                 </button>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     )
 }
