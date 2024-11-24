cdf7c29eb80cb4685ac3b52f08c3db5973d0ca9c
Kirill Seleznev <kirill011@yahoo.com>
2024-11-24 23:57:05 +0300

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
             {/* Here we add our button with alert callback */}
             <div className="card">
                 <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-                    Show Alert
+                    Show Alert!
                 </button>
             </div>
         </>
