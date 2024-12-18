00851914b4d5cfac6ff5feb4b0d746263e5d0318
Kirill Seleznev <kirill011@yahoo.com>
2024-12-19 00:12:20 +0300

    Added style from Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import {TonConnectButton} from "@tonconnect/ui-react";
 
 export const Header = () => {
     return <header>
-        <span>My App with React UI</span>
+        <span style={{display:"block"}}>My App with React UI</span>
         <TonConnectButton className="btn" style={{float:"right"}} />
     </header>
 }
