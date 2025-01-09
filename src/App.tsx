8bd5489e89fabb632be251acad8f46485f0b9bd1
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-09 15:42:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,24 @@
-import {useState, useRef, useEffect, useMemo} from 'react';
+import {useState} from 'react';
 import { Card } from 'flowbite-react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import {TonConnectUIProvider} from '@tonconnect/ui-react'
+import {Header} from "./components/Header/Header"
 import WebApp from '@twa-dev/sdk'
 
 function App() {
     const [count, setCount] = useState(0);
-
-    const searchParams = useMemo(
-        () => new URLSearchParams(document.location.search),
-        []
-    );
-    
-    const [id, setId] = useState<string | null>("");
-    const [firstName, setFirstName] = useState<string | null>("");
-    const [lastName, setLastName] = useState<string | null>("");
-    
-    const telegramWrapperRef = useRef(null);
-    
-    useEffect(() => {
-        const scriptElement = document.createElement("script");
-        scriptElement.src = "https://telegram.org/js/telegram-widget.js?22";
-        scriptElement.setAttribute("data-telegram-login", "TMAppsBot");
-        scriptElement.setAttribute("data-size", "large");
-        scriptElement.setAttribute("data-auth-url", "");
-        scriptElement.setAttribute("data-request-access", "write");
-        scriptElement.async = true;
-        
-        telegramWrapperRef.current.appendChild(scriptElement);
-    }, []);
-        
-    useEffect(() => {
-        setId(searchParams.get("id"));
-        setFirstName(searchParams.get("first_name"));
-        setLastName(searchParams.get("last_name"));
-    }, [searchParams]);
     
     return (
-        <>
-            {telegramWrapperRef}
-            {id || firstName || lastName ? (
-                <Card className="max-w-sm">
-                    <h5 className="text-xl text-left font-bold tracking-tight text-gray-900 dark:text-white">
-                        Data from Telegram redirecting
-                    </h5>
-                    <p className="font-normal text-left text-gray-700 dark:text-gray-400">
-                        <span className="font-bold">ID: </span> {id ? id : "-"}
-                    </p>
-                    <p className="font-normal text-left text-gray-700 dark:text-gray-400">
-                        <span className="font-bold">First name: </span> {firstName ? firstName : "-"}
-                    </p>
-                    <p className="font-normal text-left text-gray-700 dark:text-gray-400">
-                        <span className="font-bold">Last name: </span> {lastName ? lastName : "-"}
-                    </p>
-                </Card>
-            ) : (
-                <></>
-            )}
+        <TonConnectUIProvider
+            manifestUrl="https://kirill-84.github.io/tma/tonconnect-manifest.json"
+            actionsConfiguration={{
+                twaReturnUrl: 'https://t.me/TMAppsBot'
+            }}
+        >
+        <div className="App">
+            <Header/>
             <div>
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo"/>
@@ -81,7 +42,8 @@ function App() {
                     Show Alert!
                 </button>
             </div>
-        </>
+        </div>
+        </TonConnectUIProvider>    
     );
 }
 
