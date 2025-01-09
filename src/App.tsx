10b5fe28d2556327d94596b7b8b9a904a1effec0
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-09 13:38:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,39 +10,32 @@ function App() {
     const [count, setCount] = useState(0)
     
     return (
-        {/* <TonConnectUIProvider
-            manifestUrl="https://kirill-84.github.io/tma/tonconnect-manifest.json"
-            actionsConfiguration={{
-                twaReturnUrl: 'https://t.me/TMAppsBot'
-            }}
-        > */}
-            <div className="App">
-                <Header/>
-                <div>
-                    <a href="https://vite.dev" target="_blank">
-                        <img src={viteLogo} className="logo" alt="Vite logo"/>
-                    </a>
-                    <a href="https://react.dev" target="_blank">
-                        <img src={reactLogo} className="logo react" alt="React logo"/>
-                    </a>
-                </div>
-                <h1>Vite + React</h1>
-                <div className="card">
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-                    <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
-                </div>
-                <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-                <p>Test from MiniApps</p>
-                {/* Here we add our button with alert callback */}
-                <div className="card">
-                    <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-                        Show Alert!
-                    </button>
-                </div>
+        <div className="App">
+            <Header/>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
             </div>
-            {/* </TonConnectUIProvider> */}
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
+            </div>
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <p>Test from MiniApps</p>
+            {/* Here we add our button with alert callback */}
+            <div className="card">
+                <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+                    Show Alert!
+                </button>
+            </div>
+        </div>
     )
 }
 
