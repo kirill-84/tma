0027f0e596189b40591872e3682122a05241c37f
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-09 15:46:02 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,6 @@ import {TonConnectButton} from "@tonconnect/ui-react"
 export const Header = () => {
     return <header>
         <span className="block">My App with React UI</span>
-        <TonConnectButton className="btn" style={{float:"right"}} />
+        <TonConnectButton className="btn float-right" />
     </header>
 }
