d9f9d4b6910c919f436f31b00f56b3c1b1a4cc3a
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-09 14:07:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import {useState} from 'react'
+import {useState, useRef, useEffect, useMemo} from 'react'
+import { Card } from 'flowbite-react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +9,34 @@ import WebApp from '@twa-dev/sdk'
 
 function App() {
     const [count, setCount] = useState(0)
+
+    const searchParams = useMemo(
+        () => new URLSearchParams(document.location.search),
+        []
+    );
+    
+    const [id, setId] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    
+    const tgWrapperRef = useRef(null)
+    
+    useEffect(() => {
+        const scriptElement = document.createElement("script")
+        scriptElement.src = "https://telegram.org/js/telegram-widget.js?22"
+        scriptElement.setAttribute("data-telegram-login", "TMAppsBot")
+        scriptElement.setAttribute("data-size", "medium")
+        scriptElement.setAttribute("data-auth-url", "")
+        scriptElement.async = true
+        
+        tgWrapperRef.current.appendChild(scriptElement)
+    }, [])
+        
+    useEffect(() => {
+        setId(searchParams.get("id"))
+        setFirstName(searchParams.get("first_name"))
+        setLastName(searchParams.get("last_name"))
+    }, [searchParams])
     
     return (
         {/* <TonConnectUIProvider
@@ -17,7 +46,28 @@ function App() {
             }}
         > */}
         <div className="App">
-            <Header/>
+        {/* <Header/> */}
+        <div className="telegram-login-widget flex justify-center mb-3" ref={tgWrapperRef}></div>
+        {id || firstName || lastName ? (
+        <Card className="max-w-sm">
+          <h5 className="text-xl text-left font-bold tracking-tight text-gray-900 dark:text-white">
+            Data from Telegram redirecting
+          </h5>
+          <p className="font-normal text-left text-gray-700 dark:text-gray-400">
+            <span className="font-bold">ID: </span> {id ? id : "-"}
+          </p>
+          <p className="font-normal text-left text-gray-700 dark:text-gray-400">
+            <span className="font-bold">First name: </span>
+            {firstName ? firstName : "-"}
+          </p>
+          <p className="font-normal text-left text-gray-700 dark:text-gray-400">
+            <span className="font-bold">Last name: </span>
+            {lastName ? lastName : "-"}
+          </p>
+        </Card>
+      ) : (
+        <></>
+      )}
             <div>
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo"/>
