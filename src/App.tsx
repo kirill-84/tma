6a9e48148d8885a950fa638bebe9a9920c94083f
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-09 17:26:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect, useMemo} from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,35 +7,7 @@ import {Header} from "./components/Header/Header"
 import WebApp from '@twa-dev/sdk'
 
 function App() {
-    const searchParams = useMemo(
-        () => new URLSearchParams(document.location.search),
-        []
-      );
-    
-    const [count, setCount] = useState(0);
-    
-  const [id, setId] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  const telegramWrapperRef = useRef(null);
-
-  useEffect(() => {
-    const scriptElement = document.createElement("script");
-    scriptElement.src = "https://telegram.org/js/telegram-widget.js?22";
-    scriptElement.setAttribute("data-telegram-login", "TMAppsBot");
-    scriptElement.setAttribute("data-size", "large");
-    scriptElement.setAttribute("data-auth-url", "");
-    scriptElement.async = true;
-
-    telegramWrapperRef.current.appendChild(scriptElement);
-  }, []);
-
-  useEffect(() => {
-    setId(searchParams.get("id"));
-    setFirstName(searchParams.get("first_name"));
-    setLastName(searchParams.get("last_name"));
-  }, [searchParams]);
+    const [count, setCount] = useState(0)
     
     return (
         <TonConnectUIProvider
