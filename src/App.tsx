59c25b7bfdcc735ebe2249c80d0fdddc337c3aa2
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-09 17:17:43 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         []
       );
     
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
     
     const [count, setCount] = useState(0);
   const [id, setId] = useState("");
