6e72aaeeb61059167ddd6caba0ecbc0f0de84076
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-01-09 14:13:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
-import {useState, useRef, useEffect, useMemo} from 'react'
-import { Card } from 'flowbite-react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import {TonConnectUIProvider} from '@tonconnect/ui-react'
-import {Header} from "./components/Header/Header"
-import WebApp from '@twa-dev/sdk'
+import {useState, useRef, useEffect, useMemo} from 'react';
+import { Card } from 'flowbite-react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+// import {TonConnectUIProvider} from '@tonconnect/ui-react';
+import {Header} from "./components/Header/Header";
+import WebApp from '@twa-dev/sdk';
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
     const searchParams = useMemo(
         () => new URLSearchParams(document.location.search),
         []
     );
     
-    const [id, setId] = useState("")
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
+    const [id, setId] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     
-    const tgWrapperRef = useRef(null)
+    const tgWrapperRef = useRef(null);
     
     useEffect(() => {
-        const scriptElement = document.createElement("script")
-        scriptElement.src = "https://telegram.org/js/telegram-widget.js?22"
-        scriptElement.setAttribute("data-telegram-login", "TMAppsBot")
-        scriptElement.setAttribute("data-size", "medium")
-        scriptElement.setAttribute("data-auth-url", "")
-        scriptElement.async = true
+        const scriptElement = document.createElement("script");
+        scriptElement.src = "https://telegram.org/js/telegram-widget.js?22";
+        scriptElement.setAttribute("data-telegram-login", "TMAppsBot");
+        scriptElement.setAttribute("data-size", "medium");
+        scriptElement.setAttribute("data-auth-url", "");
+        scriptElement.async = true;
         
-        tgWrapperRef.current.appendChild(scriptElement)
-    }, [])
+        tgWrapperRef.current.appendChild(scriptElement);
+    }, []);
         
     useEffect(() => {
-        setId(searchParams.get("id"))
-        setFirstName(searchParams.get("first_name"))
-        setLastName(searchParams.get("last_name"))
-    }, [searchParams])
+        setId(searchParams.get("id"));
+        setFirstName(searchParams.get("first_name"));
+        setLastName(searchParams.get("last_name"));
+    }, [searchParams]);
     
     return (
         {/* <TonConnectUIProvider
