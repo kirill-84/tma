537baa5aab0380a8b763470afd639731b169d713
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-18 15:48:32 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,21 @@
-import {TonConnectButton, useTonConnectUI} from "@tonconnect/ui-react"
-const [tonConnectUI] = useTonConnectUI();
+import {TonConnectButton, Locales, useTonConnectUI} from "@tonconnect/ui-react"
 
-const transaction = {
-    messages: [
-        {
-            address: "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
-            amount: "20000000" //Toncoin in nanotons
-        }
-    ]
+const [tonConnectUI, setOptions] = useTonConnectUI();
 
-}
+const onLanguageChange = (language: Locales) => {
+    setOptions({ language });
+};
 
 export const Header = () => {
-    const [tonConnectUI, setOptions] = useTonConnectUI();
-    
     return <header>
         <span className="block">My App with React UI!</span>
         <TonConnectButton className="btn float-right" />
-
         <div>
-            <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
-                Send transaction
-            </button>
+          <label>language</label>
+          <select onChange={(e) => onLanguageChange(e.target.value as Locales)}>
+            <option value="en">en</option>
+            <option value="ru">ru</option>
+          </select>
         </div>
     </header>
 }
