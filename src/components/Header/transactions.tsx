960aceee21d9f8c8f9999705ec9ee96794cf0be8
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-18 12:54:52 +0300

    Update transactions.tsx

--- src/components/Header/transactions.tsx
+++ src/components/Header/transactions.tsx
@@ -1,24 +1,24 @@
 import { useTonConnectUI } from '@tonconnect/ui-react';
+const [tonConnectUI] = useTonConnectUI();
 
-const transaction: SendTransactionRequest = {
-  validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes
-  messages: [
-    {
-      address:
-        "UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA", // message destination in user-friendly format
-      amount: "50000000", // Toncoin in nanotons
-    },
-  ],
-};
+const transaction = {
+    messages: [
+        {
+            address: "UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA", // destination address
+            amount: "20000000" //Toncoin in nanotons
+        }
+    ]
+
+}
 
 export const Settings = () => {
-  const [tonConnectUI, setOptions] = useTonConnectUI();
+    const [tonConnectUI, setOptions] = useTonConnectUI();
 
-  return (
-    <div>
-      <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
-        Send transaction
-      </button>
-    </div>
-  );
+    return (
+        <div>
+            <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
+                Send transaction
+            </button>
+        </div>
+    );
 };
