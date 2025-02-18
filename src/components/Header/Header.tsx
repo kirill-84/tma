5fbb1f8fd2ceee00fb9485ae4a27795066a228b3
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-18 13:43:08 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,27 @@
 import {TonConnectButton} from "@tonconnect/ui-react"
+const [tonConnectUI] = useTonConnectUI();
+
+const transaction = {
+    messages: [
+        {
+            address: "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
+            amount: "20000000" //Toncoin in nanotons
+        }
+    ]
+
+}
 
 export const Header = () => {
+    const [tonConnectUI, setOptions] = useTonConnectUI();
+    
     return <header>
         <span className="block">My App with React UI!</span>
         <TonConnectButton className="btn float-right" />
+
+        <div>
+            <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
+                Send transaction
+            </button>
+        </div>
     </header>
 }
