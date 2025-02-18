671f19ff55f0aa8e3aba5f95fca2726dd12ed5ae
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-18 13:13:37 +0300

    Update transactions.tsx

--- src/components/Header/transactions.tsx
+++ src/components/Header/transactions.tsx
@@ -1,24 +1,28 @@
-import { useTonConnectUI } from '@tonconnect/ui-react';
-const [tonConnectUI] = useTonConnectUI();
+    import { useTonConnectUI } from '@tonconnect/ui-react';
+	import { toNano } from '@ton/ton'
 
-const transaction = {
+
+    const jettonWalletContract = Address.parse('UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA');
+
+    const myTransaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 360,
     messages: [
-        {
-            address: "UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA", // destination address
-            amount: "20000000" //Toncoin in nanotons
-        }
+  {
+    address: jettonWalletContract, // sender jetton wallet
+    amount: toNano("0.05").toString(), // for commission fees, excess will be returned
+    payload: body.toBoc().toString("base64") // payload with jetton transfer and comment body
+  }
     ]
+  }
 
-}
-
-export const Settings = () => {
+    export const Settings = () => {
     const [tonConnectUI, setOptions] = useTonConnectUI();
 
     return (
-        <div>
-            <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
-                Send transaction
-            </button>
-        </div>
-    );
+    <div>
+    <button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
+    Send transaction
+  </button>
+</div>
+);
 };
