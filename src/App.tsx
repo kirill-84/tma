1a6786075b4bb132ef41ed06b7f7a1e75a3b74cc
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-18 11:28:16 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {TonConnectUIProvider} from '@tonconnect/ui-react'
-import {Header} from "./components/Header/Header"
+import {Header, TonConnect} from "./components/Header"
 import WebApp from '@twa-dev/sdk'
 
 function App() {
@@ -34,7 +34,7 @@ function App() {
                 <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-            <p>Test from MiniApps</p>
+            <TonConnect/>
             {/* Here we add our button with alert callback */}
             <div className="card">
                 <button className="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
