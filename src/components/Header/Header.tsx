09368d649d30ab827d04bc67f1370f404b18c74c
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-18 13:36:48 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,4 @@
 import {TonConnectButton} from "@tonconnect/ui-react"
-import { Address, TonClient, beginCell, StateInit, storeStateInit } from '@ton/ton'
-
-async function main() {
-  const client = new TonClient({
-  endpoint: 'https://toncenter.com/api/v2/jsonRPC',
-  apiKey: 'f1ba5757332be5a6b004f125117f93cdb3a5e8bcd7bea50a50fe86f77229f6fd'
-})
-
-const jettonWalletAddress = Address.parse('UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA');
-  let jettonWalletDataResult = await client.runMethod(jettonWalletAddress, 'get_wallet_data');
-  jettonWalletDataResult.stack.readNumber();
-  const ownerAddress = jettonWalletDataResult.stack.readAddress();
-  const jettonMasterAddress = jettonWalletDataResult.stack.readAddress();
-  const jettonCode = jettonWalletDataResult.stack.readCell();
-  const jettonData = beginCell()
-  .storeCoins(0)
-  .storeAddress(ownerAddress)
-  .storeAddress(jettonMasterAddress)
-  .storeRef(jettonCode)
-  .endCell();
-
-  const stateInit: StateInit = {
-  code: jettonCode,
-  data: jettonData
-}
-
-const stateInitCell = beginCell()
-  .store(storeStateInit(stateInit))
-  .endCell();
-
-  console.log(new Address(0, stateInitCell.hash()));
-}
 
 export const Header = () => {
     return <header>
