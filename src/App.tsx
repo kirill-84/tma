7e9d6c40abac48b97ce17bf8a8f582bc59f831e7
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-02-20 10:38:21 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
                 </button>
             </div>
             <div className="mt-2">
-                For inspiration: 0x1266...6d63 <svg onClick={() => copyText("0x1266a05165458ce7b2C120FB744887f40eca6d63")} className="inline-block cursor-pointer" width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><title>Copy to clipboard</title>
+                For inspiration: 0x1266...6d63 <svg onClick={() => copyText("0x1266a05165458ce7b2C120FB744887f40eca6d63")} className="inline-block cursor-pointer" width="21px" height="21px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><title>Copy to clipboard</title>
 <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5 16.5L19.5 4.5L18.75 3.75H9L8.25 4.5L8.25 7.5L5.25 7.5L4.5 8.25V20.25L5.25 21H15L15.75 20.25V17.25H18.75L19.5 16.5ZM15.75 15.75L15.75 8.25L15 7.5L9.75 7.5V5.25L18 5.25V15.75H15.75ZM6 9L14.25 9L14.25 19.5L6 19.5L6 9Z" fill="#080341"/>
 </svg> | &hearts; Ethereum
             </div>
