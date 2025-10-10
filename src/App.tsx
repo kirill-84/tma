bc2fd4b39a32e82782cca9fe1f6396e291d7ea1f
Kirill <35264591+kirill-84@users.noreply.github.com>
2025-10-10 09:25:12 +0300

    Update my addr TON

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
                 </button>
             </div>
             <div className="mt-2">
-                For inspiration: UQCT...ESYA <svg onClick={() => copyText("UQCTmlmrH5rZfFN-kFsvFl0TwyF6OS5sp3QEn8nNWnW0ESYA")} className="inline-block cursor-pointer" width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><title>Copy to clipboard</title>
+                For inspiration: UQBw...o5FI <svg onClick={() => copyText("UQBwJX9X7no3RSaiHM9dke-3sI-tS37IK_zUmvhOrzU8o5FI")} className="inline-block cursor-pointer" width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><title>Copy to clipboard</title>
 <path fill-rule="evenodd" clip-rule="evenodd" d="M19.5 16.5L19.5 4.5L18.75 3.75H9L8.25 4.5L8.25 7.5L5.25 7.5L4.5 8.25V20.25L5.25 21H15L15.75 20.25V17.25H18.75L19.5 16.5ZM15.75 15.75L15.75 8.25L15 7.5L9.75 7.5V5.25L18 5.25V15.75H15.75ZM6 9L14.25 9L14.25 19.5L6 19.5L6 9Z" fill="#00BFFF"/>
 </svg> | &hearts; Toncoin
             </div>
